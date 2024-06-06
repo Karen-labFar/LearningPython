d3ba3ea8f0fa711b38c049b512fb5b534327dc37
Karen-labFar <karenrafaela7939@gmail.com>
2024-06-06 14:13:23 -0300

    Primeiro envio -Apresentação

---
+++ OlaMundo.py
@@ -0,0 +1,7 @@
+nome = 'Karen'
+idade = int(25)
+cidade = 'Aruja'
+formacao = 'Mestre em ciencias'
+
+print (f'Ola! Meu nome e {nome}, tenho {idade} anos, moro na cidade de {cidade} e sou {formacao}')
+
