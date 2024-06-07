30ee286ac7c9be97ae8c88e8f3f8e8217869068e
Karen Rafaela <karenrafaela7939@gmail.com>
2024-06-07 16:52:32 -0300

    Aula 4 Python If, Else, Elif

---
+++ Aula_4.py
@@ -0,0 +1,11 @@
+#ESTRUTURA CONDICIONAL
+
+#Nota= 8
+
+Resultadofinal= float(input('Qual foi a sua nota final? '))
+
+if Resultadofinal >=8:
+    print ('Uhul voce foi aprovado')
+else:
+    print('Vai precisar de recuperacao')
+    
